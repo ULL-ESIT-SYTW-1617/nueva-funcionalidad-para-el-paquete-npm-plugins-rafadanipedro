ecc856422e5d5cbd7094f21ce24bf9a85bba803b
Rafa Herrero <rafael.herrero.13@ull.edu.es>
2016-10-19 19:19:26 +0000

    Avanzando

--- app/generarGitbook.sh
+++
@@ -1,4 +0,0 @@
-cd app/gitbook
-../../node_modules/gitbook-cli/bin/gitbook.js install
-../../node_modules/gitbook-cli/bin/gitbook.js build
-cd ../..
---
+++ plugin-iaas-rafadanipedro.js
@@ -0,0 +1,24 @@
+import gulp from 'gulp'
+import GulpSSH from 'gulp-ssh'
+import fs from 'fs-promise'
+
+let ruta = "/home/usuario/Practica4/gh-pages"
+let dir_maq = "10.6.128.185"
+let usuario = "usuario"
+
+var config = {
+  host: dir_maq,
+  port: 22,
+  username: usuario,
+  privateKey: fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)
+}
+
+var gulpSSH = new GulpSSH({
+  ignoreErrors: false,
+  sshConfig: config
+})
+
+export default function deployIaas() {
+  return gulpSSH
+    .shell(['cd ' + ruta, 'git pull'])
+}
