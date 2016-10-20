7f3fcb3e79bdfa9b77453c65d6814cbee3aff7ae
Rafa Herrero <rafael.herrero.13@ull.edu.es>
2016-10-20 17:17:16 +0000

    Cambiando configuracion plugin

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
-import gulp from 'gulp'
-import GulpSSH from 'gulp-ssh'
-import fs from 'fs-promise'
-import prompt from 'gulp-prompt'
+'use strict';
 
-let ruta = "/home/usuario/Practica4/gh-pages"
-let dir_maq = "10.6.128.185"
-let usuario = "usuario"
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.deploy = deploy;
+
+var _gulp = require('gulp');
+
+var _gulp2 = _interopRequireDefault(_gulp);
+
+var _gulpSsh = require('gulp-ssh');
+
+var _gulpSsh2 = _interopRequireDefault(_gulpSsh);
+
+var _fsPromise = require('fs-promise');
+
+var _fsPromise2 = _interopRequireDefault(_fsPromise);
+
+var _gulpPrompt = require('gulp-prompt');
+
+var _gulpPrompt2 = _interopRequireDefault(_gulpPrompt);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var ruta = "/home/usuario/Practica4/gh-pages";
+var dir_maq = "10.6.128.185";
+var usuario = "usuario";
 
 var config = {
   host: dir_maq,
   port: 22,
   username: usuario,
-  privateKey: fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)
-}
+  privateKey: _fsPromise2.default.readFileSync(process.env.HOME + '/.ssh/id_rsa')
+};
 
-var gulpSSH = new GulpSSH({
+var gulpSSH = new _gulpSsh2.default({
   ignoreErrors: false,
   sshConfig: config
-})
+});
 
-export function deploy(args) {
-  return gulpSSH
-    .shell(['cd ' + ruta, 'git pull'])
+function deploy(args) {
+  return gulpSSH.shell(['cd ' + ruta, 'git pull']);
 
-  return through.obj(function(file, encoding, callback) {
+  return through.obj(function (file, encoding, callback) {
     callback(null, doSomethingWithTheFile(file));
   });
 }
---
+++ plugin-iaas-rafadanipedro.js
@@ -0,0 +1,29 @@
+import gulp from 'gulp'
+import GulpSSH from 'gulp-ssh'
+import fs from 'fs-promise'
+import prompt from 'gulp-prompt'
+
+let ruta = "/home/usuario/Practica4/gh-pages"
+let dir_maq = "10.6.128.185"
+let usuario = "usuario"
+
+var config = {
+  host: dir_maq,
+  port: 22,
+  username: usuario,
+  privateKey: fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)
+}
+
+var gulpSSH = new GulpSSH({
+  ignoreErrors: false,
+  sshConfig: config
+})
+
+export function deploy(args) {
+  return gulpSSH
+    .shell(['cd ' + ruta, 'git pull'])
+
+  return through.obj(function(file, encoding, callback) {
+    callback(null, doSomethingWithTheFile(file));
+  });
+}
